7778088ec9ee4b7c3f7de9a63af6a93f0b0eea82
NickWise <nickwise2020@hotmail.com>
2026-06-04 20:38:20 -0500

    Configure Cors

--- backend/ResumeAnalyzerAPI/Program.cs
+++ backend/ResumeAnalyzerAPI/Program.cs
@@ -13,6 +13,18 @@ builder.Services.AddDbContext<ResumeAnalyzerDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
+const stirng AllowedOrigins = "_ReactApp";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(AllowedOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // Vite default port
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,13 +33,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.useCors(AllowedOrigins);
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.Run();
